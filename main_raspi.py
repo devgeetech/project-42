c1cf394f13b7afe012eedf57ffe30567b6b5000e
shinesanthosh <53941514+shinesanthosh@users.noreply.github.com>
2020-02-14 20:53:51 +0530

    Create main_raspi.py

---
+++ main_raspi.py
@@ -0,0 +1,22 @@
+#import serial
+import lightcontrol
+import time
+
+
+state = [0,0,0,0]
+retime = lightcontrol.control([3,1,1,1])
+ways = len(retime)
+
+while True:
+    for i in range(0,ways):
+        state[i] = 2
+        lightcontrol.light(state)
+        time.sleep(retime[i])
+        state[i] = 1
+        lightcontrol.light(state)
+        if retime[i] == 0:
+            time.sleep(0)
+        else:
+            time.sleep(3)
+        state[i] = 0
+        lightcontrol.light(state)
