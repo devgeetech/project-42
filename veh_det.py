52fafcf30a9772e0dec73315212bd580c1d8adfe
devgeetech <devgeetech@gmail.com>
2020-02-14 21:39:59 +0530

    camera sources 4

--- veh_det.py
+++ veh_det.py
@@ -9,20 +9,38 @@ import cvlib as cv
 import matplotlib.pyplot as plt
 
 def getVehs():
-     cam1 = cv2.VideoCapture("3.avi")
-     success, image = cam1.read()
-     #if success:
-          #cv2.imwrite("first_frame.jpg", image)
+     cam1 = cv2.VideoCapture("1.avi")
+     success1, image1 = cam1.read()
      cam1.release()
-     bbox, label, conf = cv.detect_common_objects(image)
+     plt.imshow(image1)
+     plt.show()
+     
+     cam2 = cv2.VideoCapture("2.avi")
+     success2, image2 = cam2.read()
+     cam2.release()
+     plt.imshow(image2)
+     plt.show()
+     
+     cam3 = cv2.VideoCapture("3.avi")
+     success3, image3 = cam3.read()
+     cam3.release()
+     plt.imshow(image3)
+     plt.show()
+     
+     cam4 = cv2.VideoCapture("4.avi")
+     success4, image4 = cam4.read()
+     cam4.release()
+     plt.imshow(image4)
+     plt.show()
+
+     bbox, label, conf = cv.detect_common_objects(image1)
      print('Number of cars in the image is '+ str(label.count('car')))
      print('Number of buses in the image is '+ str(label.count('bus')))
      print('Number of motorcycle in the image is '+ str(label.count('motorcycle')))
      totveh = label.count('car') + label.count('bus') + label.count('motorcycle')
      totwei = label.count('car') + (label.count('bus')*3) + label.count('motorcycle')
-     plt.imshow(image)
-     plt.show()
+
      return ([label.count('car') ,label.count('bus'), label.count('motorcycle'), totveh, totwei])
      #return(label.count('car'))
 
-#getVehs()7
+getVehs()
