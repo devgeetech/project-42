a866c69a8f7672de79deb005e1df4ebca2d022c3
Shine Santhosh <53941514+shinesanthosh@users.noreply.github.com>
2020-03-30 17:38:52 +0530

    Merged files for raspi implementation

--- lightcontrol.py
+++ lightcontrol.py
@@ -1,4 +1,17 @@
-#import serial
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+lightstate = [[1,0,0],[1,0,0],[1,0,0],[1,0,0]]
+pin = [[3,5,7],[8,10,11],[12,13,15],[16,18,19]]
+
+for i in range(0,len(pin)):
+    for j in range(0,3):
+        GPIO.setup(pin[i][j], GPIO.OUT, initial=GPIO.LOW)
 
 def control(veh, totwei, maxlim):
 
@@ -30,8 +43,32 @@ def light(state):
     for i in range(0,len(state)):
         if state[i] == 2:
             output.append('g')
+            lightstate[i] = [0,0,1]
         elif state[i] == 1:
             output.append('y')
+            lightstate[i] = [0,1,0]
+        # elif state[i] == 3: #To blink alternate red and yellow lights
+        #     blink()
         else:
             output.append('r')
+            lightstate[i] = [1,0,0]
     print(output)
+    # print lightstate
+    setlight(lightstate,pin)
+
+def setlight(listate,pino):
+    for i in range(0,len(pino)):
+        for j in range(0,3):
+            if listate[i][j] == 1:
+                GPIO.output(pino[i][j], GPIO.HIGH)
+            else:
+                GPIO.output(pino[i][j], GPIO.LOW)
+                
+
+def blink():
+    lightstate = [[1,0,0],[0,1,0],[1,0,0],[0,1,0]]
+    setlight(lightstate,pin)
+    time.sleep(1)
+    lightstate = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+    setlight(lightstate,pin)
+    time.sleep(1)
